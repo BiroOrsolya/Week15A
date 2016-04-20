7bc805bd58c1068924e5d126e76b5e6b53e7cd44
biroorsolya <birorso@gmail.com>
2016-04-20 18:54:33 +0200

    StreamReader/Writer

---
+++ StrReadWrite/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace StrReadWrite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StreamReader sr = new StreamReader(@"C:\boot.ini");
+            StreamWriter sw = new StreamWriter("boot-utf7.txt", false, Encoding.UTF7);
+
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
